97e237cfeb561fccbbb62922a9f3dc95b22f2d49
Tom Rooker <tomdrooker@gmail.com>
2018-01-23 22:00:02 +0000

    Our work carousel implemented.

---
+++ resources/js/carousels.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+      $('.your-class').slick({
+        dots: true,
+        arrows: false,
+      });
+    });
+
+    $(document).ready(function(){
+          $('.header-carousel').slick({
+            autoplay: true,
+            arrows: false,
+          });
+        });
